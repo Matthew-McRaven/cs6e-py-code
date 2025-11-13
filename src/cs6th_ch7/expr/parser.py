f6ca689053d0f361ac7e1a0ed24b88bf6b846ede
Matthew McRaven <matthew.mcraven@gmail.com>
2025-11-12 21:11:39 -0800

    Assign return value of recursive descent calls to a local
    Having the recursive call AND unpacking the value in the return was too hard to explain in prose.
    I feel this modification is less pythonic (in that I would write the current version in real code).
    
    However, the prose is substantially easier to write for this revised version.

--- src/cs6th_ch7/expr/parser.py
+++ src/cs6th_ch7/expr/parser.py
@@ -8,26 +8,26 @@ from .lexer import Lexer
 
 class ExpressionParser:
     def __init__(self, buffer: io.StringIO):
-        self.lexer = Lexer(buffer)
-        self._buffer = ParserBuffer(self.lexer)
+        self._lexer = Lexer(buffer)
+        self._buffer = ParserBuffer(self._lexer)
 
     def F(self) -> list[Token]:
         if self._buffer.may_match(tokens.ParenOpen):
-            E = self.E()
+            e = self.E()
             self._buffer.must_match(tokens.ParenClose)
-            return [*E]
-        elif token := self._buffer.must_match(tokens.Decimal):
-            return [token]
-        raise SyntaxError()
+            return e
+        return [self._buffer.must_match(tokens.Decimal)]
 
     def T(self) -> list[Token]:
         f = self.F()
         if times := self._buffer.may_match(tokens.Times):
-            return [*f, *self.T(), times]
-        return [*f]
+            t = self.T()
+            return [*f, *t, times]
+        return f
 
     def E(self) -> list[Token]:
         t = self.T()
         if plus := self._buffer.may_match(tokens.Plus):
-            return [*t, *self.E(), plus]
-        return [*t]
+            e = self.E()
+            return [*t, *e, plus]
+        return t
