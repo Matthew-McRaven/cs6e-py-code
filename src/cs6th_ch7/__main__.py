2d359a77f9c3532a5070a21887fde48c9645e963
Matthew McRaven <matthew.mcraven@gmail.com>
2025-11-16 12:23:12 -0800

    Update descriptions for subcommands
    Parser descriptions have been updated to reference the figures in the book.

--- src/cs6th_ch7/__main__.py
+++ src/cs6th_ch7/__main__.py
@@ -95,13 +95,22 @@ def exec_expr(args):
     parser = ExpressionParser(buffer)
     expr_tokens = parser.E()
     print(expression_string(expr_tokens))
+
+def exec_expr_codegen(args):
+    text = text_from_args(args)
+    # Remove trailing whitespace while insuring input is \n terminated.
+    buffer = io.StringIO(text.rstrip() + "\n")
+    parser = ExpressionParser(buffer)
+    expr_tokens = parser.E()
     ir_lines = to_pep10_ir(expr_tokens)
     ir, ir_errors = calculate_addresses(ir_lines)
     if len(ir_errors) > 0:
         for ir_error in ir_errors:
             print(ir_error, file=sys.stderr)
         raise SyntaxError("Failed to generate object code")
-    print("".join(hex(x)[2:] for x in program_object_code(ir)))
+    print("\n".join(x for x in program_listing(ir)))
+
+
 
 
 def exec_parser(args):
@@ -119,9 +128,9 @@ def exec_codegen(args):
 
 def main():
     parser = argparse.ArgumentParser()
-    subparsers = parser.add_subparsers(required=True)
+    subparsers = parser.add_subparsers()
     parse_table = subparsers.add_parser(
-        "table", help="CLI for table-lookup FSM of Figure 7.28"
+        "table", help="A table-lookup FSM to recognize identifiers (Figure 7.28)"
     )
 
     parse_table.set_defaults(func=exec_fsm_table)
@@ -130,7 +139,7 @@ def main():
     parse_table_group.add_argument("--file")
 
     parse_fsm_direct = subparsers.add_parser(
-        "direct", help="CLI for direct-coded FSM of Figure 7.29"
+        "direct", help="A direct-coded FSM to scan signed decimals (Figure 7.29)"
     )
     parse_fsm_direct.set_defaults(func=exec_fsm_direct)
     parse_direct_group = parse_fsm_direct.add_mutually_exclusive_group(
@@ -140,21 +149,21 @@ def main():
     parse_direct_group.add_argument("--file")
 
     parse_fsm_hex = subparsers.add_parser(
-        "hexdirect", help="CLI for direct-coded FSM to scan hex string"
+        "hexdirect", help="A direct-coded FSM to scan hex strings (Problem 7.18)"
     )
     parse_fsm_hex.set_defaults(func=exec_fsm_hex)
     parse_hex_group = parse_fsm_hex.add_mutually_exclusive_group(required=True)
     parse_hex_group.add_argument("--text")
     parse_hex_group.add_argument("--file")
 
-    parse_lex = subparsers.add_parser("lex", help="Test bench for Pepp lexer")
+    parse_lex = subparsers.add_parser("lex", help="A multiple-token lexical analyzer for Pep/10 (Figure 7.34")
     parse_lex.set_defaults(func=exec_lex)
     parse_lex_group = parse_lex.add_mutually_exclusive_group(required=True)
     parse_lex_group.add_argument("--text")
     parse_lex_group.add_argument("--file")
 
     parse_expr = subparsers.add_parser(
-        "expr", help="Test bench for Expression Parser"
+        "expr", help="A recursive descent parser for an expression grammar (Figure 7.41)"
     )
     parse_expr.set_defaults(func=exec_expr)
     parse_expr_group = parse_expr.add_mutually_exclusive_group(required=True)
@@ -162,7 +171,7 @@ def main():
     parse_expr_group.add_argument("--file")
 
     parse_parser = subparsers.add_parser(
-        "parser", help="Test bench for Pepp parser"
+        "parser", help="A recursive descent parser for Pep/10 (Figure 7.49)"
     )
     parse_parser.set_defaults(func=exec_parser)
     parse_parser_group = parse_parser.add_mutually_exclusive_group(
@@ -172,7 +181,7 @@ def main():
     parse_parser_group.add_argument("--file")
 
     parse_codegen = subparsers.add_parser(
-        "codegen", help="Test bench for Pepp code generation"
+        "codegen", help="A code generator for Pep/10 (Figure 7.xx)"
     )
     parse_codegen.set_defaults(func=exec_codegen)
     parse_codegen_group = parse_codegen.add_mutually_exclusive_group(
@@ -180,6 +189,17 @@ def main():
     )
     parse_codegen_group.add_argument("--text")
     parse_codegen_group.add_argument("--file")
+
+    parse_compile = subparsers.add_parser(
+        "exprcode", help="A code generator for an expression grammar (Figure 7.xx)"
+    )
+    parse_compile.set_defaults(func=exec_expr_codegen)
+    parse_compile = parse_compile.add_mutually_exclusive_group(required=True)
+    parse_compile.add_argument("--text")
+    parse_compile.add_argument("--file")
+
+    # Print help by default if no subcommand is provided
+    parser.set_defaults(func=lambda _: parser.print_help())
 
     args = parser.parse_args()
     return args.func(args)
